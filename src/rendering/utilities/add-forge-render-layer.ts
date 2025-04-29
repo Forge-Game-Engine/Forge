f9c979003859f1eba93aead3ee167857b819db61
Storm <stormmuller2@gmail.com>
2025-04-29 21:26:41 +0200

    feat: add addForgeRenderLayer and addRiveRenderLayer functions for rendering layers

---
+++ src/rendering/utilities/add-forge-render-layer.ts
@@ -0,0 +1,37 @@
+import { Entity, type World } from '../../ecs';
+import { RenderableBatchComponent } from '../components';
+import type { LayerService } from '../layer-service';
+import { ForgeRenderLayer } from '../render-layers';
+import { RenderSystem, SpriteBatchingSystem } from '../systems';
+import { createCanvas } from './create-canvas';
+
+export function addForgeRenderLayer(
+  layerName: string,
+  gameContainer: HTMLElement,
+  layerService: LayerService,
+  world: World,
+  cameraEntity: Entity,
+) {
+  const canvas = createCanvas(`$forge-layer-${layerName}`, gameContainer);
+  const layer = new ForgeRenderLayer(layerName, canvas);
+
+  layerService.registerLayer(layer);
+
+  const layerRenderSystem = new RenderSystem({
+    layer,
+    cameraEntity,
+  });
+
+  world.addSystem(layerRenderSystem);
+
+  const batcherEntity = new Entity('renderable batcher', [
+    new RenderableBatchComponent(layer),
+  ]);
+
+  const batchingSystem = new SpriteBatchingSystem(batcherEntity);
+
+  world.addEntity(batcherEntity);
+  world.addSystem(batchingSystem);
+
+  return [layer, canvas] as const;
+}
