76946476fe85d419bda6c17e312521d187de22d6
Storm <stormmuller2@gmail.com>
2025-04-29 21:53:58 +0200

    feat: implement Stoppable interface in RiveRenderLayer class

--- src/rendering/render-layers/rive-render-layer.ts
+++ src/rendering/render-layers/rive-render-layer.ts
@@ -7,11 +7,12 @@ import {
 } from '@rive-app/webgl2';
 import { RenderLayer } from './render-layer';
 import { EventDispatcher, ParameterizedForgeEvent } from '../../events';
+import type { Stoppable } from '../../common';
 
 /**
  * The `RiveRenderLayer` class represents a rendering layer with its own canvas and rive instance.
  */
-export class RiveRenderLayer extends RenderLayer {
+export class RiveRenderLayer extends RenderLayer implements Stoppable {
   /** The Rive instance associated with the render layer. */
   public rive: Rive;
 
