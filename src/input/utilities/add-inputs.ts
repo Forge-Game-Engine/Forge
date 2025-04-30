df00a85a7e80e3a299972e9e9cbd01141e268a76
Storm <stormmuller2@gmail.com>
2025-04-30 06:18:17 +0000

    feat: add functions to create and register multiple Forge render layers with the layer service

--- src/input/utilities/add-inputs.ts
+++ src/input/utilities/add-inputs.ts
@@ -2,6 +2,12 @@ import { Entity, type World } from '../../ecs';
 import { InputsComponent } from '../components';
 import { InputSystem } from '../systems';
 
+/**
+ * Adds an `InputsComponent` to the world and initializes the input system.
+ * @param world - The world to which the inputs component will be added.
+ * @param container - The HTML element that will receive input events.
+ * @returns The entity that contains the `InputsComponent`.
+ */
 export function addInputs(world: World, container: HTMLElement) {
   const inputsEntity = new Entity('inputs', [new InputsComponent()]);
 
--- src/rendering/utilities/index.ts
+++ src/rendering/utilities/index.ts
@@ -1,5 +1,6 @@
 export * from './create-canvas';
 export * from './add-forge-render-layer';
+export * from './add-forge-render-layers';
 export * from './add-rive-render-layer';
 export * from './add-camera';
 export * from './create-sprite';
