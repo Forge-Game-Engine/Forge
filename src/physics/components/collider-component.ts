d126bf13b82f6705b01f562fda8003f5054bcdba
Storm <stormmuller2@gmail.com>
2025-05-04 07:09:40 +0000

    refactor: rename BoxColliderComponent to ColliderComponent and update related properties

--- src/physics/components/collider-component.ts
+++ src/physics/components/collider-component.ts
@@ -2,25 +2,24 @@ import type { Component } from '../../ecs';
 import { BoxCollider as Collider } from '../colliders';
 
 /**
- * The `BoxColliderComponent` class implements the `Component` interface and represents
- * a component that contains a `BoxCollider`.
+ * The `ColliderComponent` class represents a component that holds a `Collider` instance.
  */
 export class ColliderComponent implements Component {
   /** The name property holds the unique symbol for this component. */
   public name: symbol;
 
-  /** The `BoxCollider` instance associated with this component. */
-  public boxCollider: Collider;
+  /** The `Collider` instance associated with this component. */
+  public collider: Collider;
 
-  /** A static symbol property that uniquely identifies the `BoxColliderComponent`. */
+  /** A static symbol property that uniquely identifies the `ColliderComponent`. */
   public static symbol = Symbol('Collider');
 
   /**
-   * Constructs a new instance of the `BoxColliderComponent` class with the given `BoxCollider`.
-   * @param boxCollider - The `BoxCollider` instance to associate with this component.
+   * Constructs a new instance of the `ColliderComponent` class with the given `Collider`.
+   * @param collider - The `Collider` instance to associate with this component.
    */
-  constructor(boxCollider: Collider) {
+  constructor(collider: Collider) {
     this.name = ColliderComponent.symbol;
-    this.boxCollider = boxCollider;
+    this.collider = collider;
   }
 }
