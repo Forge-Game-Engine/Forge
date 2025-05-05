709fbfa507566aca413465c84f449e96ad2622d7
Storm <stormmuller2@gmail.com>
2025-05-05 19:42:12 +0000

    feat: refactor createSprite to use Material and add createImageSprite utility

--- src/rendering/utilities/index.ts
+++ src/rendering/utilities/index.ts
@@ -3,5 +3,6 @@ export * from './add-forge-render-layer';
 export * from './add-forge-render-layers';
 export * from './add-rive-render-layer';
 export * from './add-camera';
+export * from './create-image-sprite';
 export * from './create-sprite';
 export * from './create-shader-store';
