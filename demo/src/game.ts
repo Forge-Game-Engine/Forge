886a6d5ce6630cef4249db90313fa8fe77a400d8
Storm <stormmuller2@gmail.com>
2025-05-05 20:16:43 +0000

    feat: refactor createShipPilotScene and createScene

--- demo/src/game.ts
+++ demo/src/game.ts
@@ -1,22 +1,11 @@
 import {
   createContainer,
-  cubicShaderInclude,
+  createShaderStore,
   Game,
   ImageCache,
-  perlinNoiseFragmentShader,
-  perlinNoiseShaderInclude,
-  quinticShaderInclude,
-  radialGradientShader,
-  radialGradientShaderInclude,
-  randomGradientShaderInclude,
   RiveCache,
-  sdfCircleShaderInclude,
-  sdfOrientedBoxShaderInclude,
-  ShaderStore,
-  spriteFragmentShader,
-  spriteVertexShader,
 } from '../../src';
-import { createPerlinNoiseScene } from './scenes';
+import { createShipPilotScene } from './scenes';
 
 export const game = new Game();
 
@@ -24,27 +13,10 @@ export const gameContainer = createContainer('forge-demo-game');
 
 export const imageCache = new ImageCache();
 export const riveCache = new RiveCache();
-export const shaderStore = new ShaderStore();
-
-shaderStore.addInclude(
-  cubicShaderInclude,
-  perlinNoiseShaderInclude,
-  quinticShaderInclude,
-  radialGradientShaderInclude,
-  randomGradientShaderInclude,
-  sdfCircleShaderInclude,
-  sdfOrientedBoxShaderInclude,
-);
-
-shaderStore.addShader(
-  radialGradientShader,
-  perlinNoiseFragmentShader,
-  spriteFragmentShader,
-  spriteVertexShader,
-);
+export const shaderStore = createShaderStore();
 
 game.registerScene(
-  await createPerlinNoiseScene(game, gameContainer, imageCache, shaderStore),
+  await createShipPilotScene(game, gameContainer, imageCache, shaderStore),
 );
 
 game.run();
--- demo/src/scenes/ship-pilot.ts
+++ demo/src/scenes/ship-pilot.ts
@@ -10,110 +10,43 @@ export async function createShipPilotScene(
   imageCache: forge.ImageCache,
   shaderStore: forge.ShaderStore,
 ) {
-  const scene = new forge.Scene('ship-pilot');
+  const { scene, world, layerService, cameraEntity, inputsEntity } =
+    forge.createScene('ship-pilot', game, gameContainer);
 
   const worldSpace = new forge.Space(
     window.innerWidth * 5,
     window.innerHeight * 5,
   );
-  const layerService = new forge.LayerService();
 
-  window.addEventListener('resize', () => {
-    layerService.resizeAllLayers();
-  });
+  const [backgroundRenderLayer, foregroundRenderLayer] =
+    forge.addForgeRenderLayers(
+      [forge.DEFAULT_LAYERS.background, forge.DEFAULT_LAYERS.foreground],
+      gameContainer,
+      layerService,
+      world,
+      cameraEntity,
+    );
 
-  const world = new forge.World();
-
-  const inputsEntity = new forge.Entity('inputs', [
-    new forge.InputsComponent(),
-  ]);
-
-  const cameraEntity = new forge.Entity('world camera', [
-    new forge.CameraComponent({
-      allowZooming: true,
-      allowPanning: true,
-      minZoom: 0.25,
-    }),
-    new forge.PositionComponent(0, 0),
-  ]);
-
-  const inputSystem = new forge.InputSystem(
-    gameContainer,
-    cameraEntity,
-    window.innerWidth,
-    window.innerHeight,
-  );
-
-  world.addEntity(inputsEntity);
-  world.addSystem(inputSystem);
-
-  const backgroundRenderLayer = addRenderLayer(
-    forge.DEFAULT_LAYERS.background,
-    gameContainer,
-    layerService,
-    world,
-    cameraEntity,
-  );
-
-  const foregroundRenderLayer = addRenderLayer(
-    forge.DEFAULT_LAYERS.foreground,
-    gameContainer,
-    layerService,
-    world,
-    cameraEntity,
-  );
-
-  const foregroundBatcher = new forge.Entity('foreground renderable batcher', [
-    new forge.RenderableBatchComponent(foregroundRenderLayer),
-  ]);
-
-  const backgroundBatcher = new forge.Entity('background renderable batcher', [
-    new forge.RenderableBatchComponent(backgroundRenderLayer),
-  ]);
-
-  const foregroundBatchingSystem = new forge.SpriteBatchingSystem(
-    foregroundBatcher,
-  );
-  const backgroundBatchingSystem = new forge.SpriteBatchingSystem(
-    backgroundBatcher,
-  );
-
-  world.addEntity(foregroundBatcher);
-  world.addEntity(backgroundBatcher);
-
-  await createShip(imageCache, foregroundRenderLayer, world, shaderStore);
+  await createShip(imageCache, foregroundRenderLayer.layer, world, shaderStore);
 
   const starfieldComponent = new StarfieldComponent(10_000, worldSpace);
 
-  const starField = new forge.Entity('starfield', [starfieldComponent]);
-
-  world.addEntity(starField);
+  world.buildAndAddEntity('starfield', [starfieldComponent]);
 
   const image = await imageCache.getOrLoad('star_small.png');
-  const material = new forge.SpriteMaterial(
-    backgroundRenderLayer.context,
-    shaderStore,
+
+  const sprite = forge.createImageSprite(
     image,
+    backgroundRenderLayer.layer,
+    shaderStore,
   );
-
-  const renderable = new forge.Renderable(
-    forge.createQuadGeometry(backgroundRenderLayer.context),
-    material,
-  );
-
-  const sprite = new forge.Sprite({
-    renderable,
-    renderLayer: backgroundRenderLayer,
-    width: image.width,
-    height: image.height,
-  });
 
   const random = new forge.Random();
 
   const createStar = () => {
     const scaleComponent = new forge.ScaleComponent(0.5, 0.5);
 
-    const starEntity = new forge.Entity('star', [
+    const starEntity = world.buildAndAddEntity('star', [
       new forge.PositionComponent(0, 0),
       scaleComponent,
       new forge.RotationComponent(0),
@@ -128,8 +61,6 @@ export async function createShipPilotScene(
       }),
       new StarComponent(random),
     ]);
-
-    world.addEntity(starEntity);
 
     return starEntity;
   };
@@ -153,44 +84,12 @@ export async function createShipPilotScene(
 
   const starfieldSystem = new StarfieldSystem(starPool);
 
-  world.addSystems([
+  world.addSystems(
     shipMovementSystem,
     starSystem,
     starfieldSystem,
     animationSystem,
-  ]);
-
-  const cameraSystem = new forge.CameraSystem(inputsEntity, game.time);
-
-  world.addEntity(cameraEntity);
-  world.addSystem(cameraSystem);
-  world.addSystem(foregroundBatchingSystem);
-  world.addSystem(backgroundBatchingSystem);
-
-  scene.registerUpdatable(world);
-  scene.registerStoppable(world);
+  );
 
   return scene;
-}
-
-function addRenderLayer(
-  layerName: string,
-  gameContainer: HTMLElement,
-  layerService: forge.LayerService,
-  world: forge.World,
-  cameraEntity: forge.Entity,
-) {
-  const canvas = forge.createCanvas(`$forge-layer-${layerName}`, gameContainer);
-  const layer = new forge.ForgeRenderLayer(layerName, canvas);
-
-  layerService.registerLayer(layer);
-
-  const layerRenderSystem = new forge.RenderSystem({
-    layer,
-    cameraEntity,
-  });
-
-  world.addSystem(layerRenderSystem);
-
-  return layer;
 }
