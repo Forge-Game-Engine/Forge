7e271d052f893deb4d4bd12d73cf72367b29cc33
Storm <stormmuller2@gmail.com>
2025-06-08 19:21:50 +0000

    fix: update baseUrl in Docusaurus config for GitHub Pages deployment

--- documentation-site/docusaurus.config.ts
+++ documentation-site/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   url: 'https://forge-game-engine.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Forge/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
