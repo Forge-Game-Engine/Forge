a90e20a117a619db04944d4efceff21f8050acb6
BrentTB <btbutkow@gmail.com>
2025-07-20 14:24:12 +0200

    refactor: control adventurer system

--- demo/src/control-adventurer-system.ts
+++ demo/src/control-adventurer-system.ts
@@ -30,22 +30,18 @@ export class ControlAdventurerSystem extends System {
         ImageAnimationComponent.symbol,
       );
 
-    const keyAnimation = {
-      [keyCodes.w]: ADVENTURER_ANIMATIONS.jump,
-      [keyCodes.a]: ADVENTURER_ANIMATIONS.run,
-      [keyCodes.d]: ADVENTURER_ANIMATIONS.run,
-      [keyCodes.space]: ADVENTURER_ANIMATIONS.attack1,
-    };
     if (this._inputComponent.keyPressed(keyCodes.w)) {
       // jump always happens immediately
-      imageAnimationComponent.setCurrentAnimation(keyAnimation[keyCodes.w]);
-    } else if (this._inputComponent.keyPressed(keyCodes.a)) {
+      imageAnimationComponent.setCurrentAnimation(ADVENTURER_ANIMATIONS.jump);
+    } else if (
+      this._inputComponent.keyPressed(keyCodes.a) ||
+      this._inputComponent.keyPressed(keyCodes.d)
+    ) {
       // run and attack happen at the end of the current animation
-      imageAnimationComponent.nextAnimationState = keyAnimation[keyCodes.a];
-    } else if (this._inputComponent.keyPressed(keyCodes.d)) {
-      imageAnimationComponent.nextAnimationState = keyAnimation[keyCodes.d];
+      imageAnimationComponent.nextAnimationState = ADVENTURER_ANIMATIONS.run;
     } else if (this._inputComponent.keyPressed(keyCodes.space)) {
-      imageAnimationComponent.nextAnimationState = keyAnimation[keyCodes.space];
+      imageAnimationComponent.nextAnimationState =
+        ADVENTURER_ANIMATIONS.attack1;
     }
   }
 }
