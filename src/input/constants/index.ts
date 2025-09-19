7a947949504cb783237ef28cf8ce38f4f872291c
Storm Muller <stormmuller2@gmail.com>
2025-09-19 13:10:21 +0200

    refactor: removed unused code

--- src/input/constants/index.ts
+++ src/input/constants/index.ts
@@ -4,4 +4,3 @@ export * from './button-moments';
 export * from './axis-measurements';
 export * from './action-reset-types';
 export * from './cursor-value-type';
-export * from './cursor-origins';
