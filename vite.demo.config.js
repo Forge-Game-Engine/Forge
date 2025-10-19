0992bca22cc4efe7867e3bc917385144f7e50b11
Storm <stormmuller2@gmail.com>
2025-10-19 15:20:09 +0000

    refactor: streamline package configuration and remove redundant files

--- vite.demo.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import baseConfig from './vite.config';
-
-export default defineConfig({
-  ...baseConfig,
-  root: path.resolve(__dirname, 'demo'),
-});
