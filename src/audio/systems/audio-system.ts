6b26982288c771993a7095c3cfc014ba3f91f059
Storm <stormmuller2@gmail.com>
2026-01-06 18:58:16 +0000

    feat: refactor audio component and system to ECS architecture

--- src/audio/systems/audio-system.ts
+++ src/audio/systems/audio-system.ts
@@ -1,45 +1,21 @@
-import { AudioComponent } from '../components/index.js';
-import { Entity, System, World } from '../../ecs/index.js';
+import { AudioEcsComponent, audioId } from '../components/index.js';
+import { EcsSystem } from '../../new-ecs/ecs-system.js';
+
+// TODO: needs an unload?
 
 /**
- * System to manage and play audio for entities with a AudioComponent.
+ * Creates a new ECS-style audio system.
+ * @param time - The Time instance.
+ * @returns An ECS system that updates animations.
  */
-export class AudioSystem extends System {
-  private readonly _world: World;
-
-  /**
-   * Creates an instance of AudioSystem.
-   */
-  constructor(world: World) {
-    super([AudioComponent], 'audio');
-
-    this._world = world;
-  }
-
-  /**
-   * Runs the audio system for a given entity.
-   * @param entity - The entity to update and play sounds for.
-   * @returns A promise.
-   */
-  public run(entity: Entity): void {
-    const audioComponent = entity.getComponentRequired(AudioComponent);
+export const createAnimationEcsSystem = (): EcsSystem<[AudioEcsComponent]> => ({
+  query: [audioId],
+  run: (result) => {
+    const [audioComponent] = result.components;
 
     if (audioComponent.playSound) {
       audioComponent.sound.play();
       audioComponent.playSound = false;
     }
-  }
-
-  /**
-   * Stops the audio system and unloads all sounds.
-   */
-  public stop(): void {
-    const allEntitiesWithAudio = this._world.queryEntities([AudioComponent]);
-
-    for (const entityWithAudio of allEntitiesWithAudio) {
-      const audioComponent =
-        entityWithAudio.getComponentRequired(AudioComponent);
-      audioComponent.sound.unload();
-    }
-  }
-}
+  },
+});
